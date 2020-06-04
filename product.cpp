f430721f995c411d01f450081d47b9427cb1e150
vamicagarg <63334333+vamicagarg@users.noreply.github.com>
2020-06-04 21:12:56 +0530

    contains homework

---
+++ product.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float num1,num2,product;
+	
+	cout<<"Enter two numbers: ";
+	cin>>num1>>num2;
+	
+	product=num1*num2;
+	
+	cout<<"Product = "<<product<<endl;
+	
+	return 0;
+	
+}
---
+++ sizeof.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	char c;
+	float f;
+	double d;
+	
+	cout<<"Size of char: "<<sizeof(c)<<" byte"<<endl;
+	cout<<"Size of int: "<<sizeof(i)<<" bytes"<<endl;
+	cout<<"Size of float: "<<sizeof(f)<<" bytes"<<endl;
+	cout<<"Size of double: "<<sizeof(d)<<" bytes"<<endl;
+	
+	return 0;
+}
